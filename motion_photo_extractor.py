8477288057e700040a759af7afa4c33116a84c1c
Pedro Esteves <14132411+estevesp@users.noreply.github.com>
2020-01-15 15:51:08 -0300

    Update motion_photo_extractor.py

--- motion_photo_extractor.py
+++ motion_photo_extractor.py
@@ -12,10 +12,13 @@ HEX_FILE_CONTENT = binascii.hexlify(BINARY_FILE_CONTENT)
 HEX_ARRAY = HEX_FILE_CONTENT.upper().split("4D6F74696F6E50686F746F5F44617461")
 
 DECODED_JPG = binascii.unhexlify(HEX_ARRAY[0])
-DECODED_MPG = binascii.unhexlify(HEX_ARRAY[1])
 
 with open(OUTPUT_JPG_FILE_NAME, "wb") as JPG_FILE:
     JPG_FILE.write(DECODED_JPG)
 
-with open(OUTPUT_MPG_FILE_NAME, "wb") as MPG_FILE:
-    MPG_FILE.write(DECODED_MPG)
+try:
+    DECODED_MPG = binascii.unhexlify(HEX_ARRAY[1])
+    with open(OUTPUT_MPG_FILE_NAME, "wb") as MPG_FILE:
+        MPG_FILE.write(DECODED_MPG)
+except:
+    print "Input file has no embeeded motion picture."
